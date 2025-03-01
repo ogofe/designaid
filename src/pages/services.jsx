ca41cc67a9fc6af8c6095a867a4968f413815436
Joel Tanko <7thogofe@gmail.com>
2025-03-01 22:37:08 +0100

    update services page

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -19,86 +19,85 @@ export const ServicesPage = ({ props }) => {
       
       <section id="services" class="services section-bg">
         <div class="container" data-aos="fade-up">
-    
-        <div class="row gy-4">
+          <div class="row gy-4">
 
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-mountain-city"></i>
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+              <div class="service-item position-relative">
+                <div class="icon">
+                  <i class="fa-solid fa-mountain-city"></i>
+                </div>
+                <h3>Industrial Construction</h3>
+                <p>
+                  We deliver robust industrial facilities that integrate innovative engineering with rigorous quality standards, ensuring peak operational performance.
+                </p>
+                <a href="/contact" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
               </div>
-              <h3>Industrial Construction</h3>
-              <p>
-                We deliver robust industrial facilities that integrate innovative engineering with rigorous quality standards, ensuring peak operational performance.
-              </p>
-              <a href="service-details.html" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
             </div>
-          </div>
 
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-arrow-up-from-ground-water"></i>
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+              <div class="service-item position-relative">
+                <div class="icon">
+                  <i class="fa-solid fa-arrow-up-from-ground-water"></i>
+                </div>
+                <h3>Remodeling</h3>
+                <p>
+                  Transform your space with our expert remodeling services that rejuvenate and modernize existing structures while maintaining their unique character.
+                </p>
+                <a href="/contact" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
               </div>
-              <h3>Remodeling</h3>
-              <p>
-                Transform your space with our expert remodeling services that rejuvenate and modernize existing structures while maintaining their unique character.
-              </p>
-              <a href="/contact" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
             </div>
-          </div>
 
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-compass-drafting"></i>
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+              <div class="service-item position-relative">
+                <div class="icon">
+                  <i class="fa-solid fa-compass-drafting"></i>
+                </div>
+                <h3>Architectural Design</h3>
+                <p>
+                  Our design team crafts visionary spaces that are both innovative and functional, tailored to your specific needs while setting new industry standards.
+                </p>
+                <a href="/contact" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
               </div>
-              <h3>Architectural Design</h3>
-              <p>
-                Our design team crafts visionary spaces that are both innovative and functional, tailored to your specific needs while setting new industry standards.
-              </p>
-              <a href="/contact" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
             </div>
-          </div>
 
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-trowel-bricks"></i>
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+              <div class="service-item position-relative">
+                <div class="icon">
+                  <i class="fa-solid fa-trowel-bricks"></i>
+                </div>
+                <h3>Estate Building</h3>
+                <p>
+                  We develop communities that blend contemporary design with sustainable living, creating estate projects that serve as modern sanctuaries.
+                </p>
+                <a href="/contact" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
               </div>
-              <h3>Estate Building</h3>
-              <p>
-                We develop communities that blend contemporary design with sustainable living, creating estate projects that serve as modern sanctuaries.
-              </p>
-              <a href="/contact" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
             </div>
-          </div>
 
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-helmet-safety"></i>
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+              <div class="service-item position-relative">
+                <div class="icon">
+                  <i class="fa-solid fa-helmet-safety"></i>
+                </div>
+                <h3>Project Management</h3>
+                <p>
+                  Our experienced project managers ensure every phase of your project is seamlessly coordinated, delivering exceptional results on time and within budget.
+                </p>
+                <a href="/contact" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
               </div>
-              <h3>Project Management</h3>
-              <p>
-                Our experienced project managers ensure every phase of your project is seamlessly coordinated, delivering exceptional results on time and within budget.
-              </p>
-              <a href="/contact" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
             </div>
-          </div>
 
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-arrow-up-from-ground-water"></i>
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
+              <div class="service-item position-relative">
+                <div class="icon">
+                  <i class="fa-solid fa-arrow-up-from-ground-water"></i>
+                </div>
+                <h3>Facility Management</h3>
+                <p>
+                  Our comprehensive facility management services ensure your property remains at peak performance, blending proactive maintenance with modern technology.
+                </p>
+                <a href="/contact" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
               </div>
-              <h3>Facility Management</h3>
-              <p>
-                Our comprehensive facility management services ensure your property remains at peak performance, blending proactive maintenance with modern technology.
-              </p>
-              <a href="/contact" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
             </div>
-          </div>
 
           </div>
         </div>
@@ -109,7 +108,6 @@ export const ServicesPage = ({ props }) => {
         <div class="container" data-aos="fade-up">
     
           <div class="row gy-4">
-    
             <div class="col-lg-3 col-md-6" data-aos="zoom-in" data-aos-delay="100">
               <h3>Quasi eaque omnis</h3>
               <p>Eius non minus autem soluta ut ui labore omnis quisquam corrupti autem odit voluptas quos commodi magnam occaecati.</p>
@@ -150,7 +148,6 @@ export const ServicesPage = ({ props }) => {
                 <li><i class="bi bi-check2"></i> <span>Voluptatem pariatur est sationem</span></li>
               </ul>
             </div>
-    
           </div>
     
         </div>
@@ -197,7 +194,7 @@ export const ServicesPage = ({ props }) => {
               </div>
             </div>
 
-            <div class="col-lg-6 img-bg" style="background-image: url(assets/img/alt-services-2.jpg);" data-aos="zoom-in" data-aos-delay="100"></div>
+            <div class="col-lg-6 img-bg" style={{ backgroundImage: "url('/assets/img/alt-services-2.jpg')"}} data-aos="zoom-in" data-aos-delay="100"></div>
           </div>
 
         </div>
